f7aec98740fa398b86387559e94ba91fbcf0b443
Aleksandr Makarov <avas.makarov@gmail.com>
2023-04-12 16:53:39 +0300

    HW4_2 done

--- Python/Learning Py.py
+++ Python/Learning Py.py
@@ -1,1 +1,14 @@
-print ('Hello world!')
+import socket,time
+
+list_service = {'drive.google.com':'0.0.0.0', 'mail.google.com':'0.0.0.0', 'google.com':'0.0.0.0'}
+while 1 == 1 :
+    for service in list_service:
+        response_ip = socket.gethostbyname(service)
+        if response_ip != list_service[service] :
+            print(' [ERROR] ' + str(service) +' IP mistmatch: '+list_service[service]+' '+response_ip)
+            list_service[service]=response_ip
+        else :
+            print(str(service) + ' ' + response_ip)
+    time.sleep(10)
+
+
