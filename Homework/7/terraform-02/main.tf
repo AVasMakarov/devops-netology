5f02810d1823c873a6adde7e67d707f1e3c7b2ae
Aleksandr Makarov <avas.makarov@gmail.com>
2023-07-12 17:00:09 +0300

    HW7_2 3 done

--- Homework/7/terraform-02/main.tf
+++ Homework/7/terraform-02/main.tf
@@ -39,3 +39,34 @@ resource "yandex_compute_instance" "terraform1" {
   }
 
 }
+
+data "yandex_compute_image" "ubuntu_db" {
+  family = var.vm_db_image
+}
+resource "yandex_compute_instance" "terraform2" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_db.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
